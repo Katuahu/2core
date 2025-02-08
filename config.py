f793e5e3978810d11ba383821eee70bce57c1fc3
Katuahu <bhadwakekdahubkl@gmail.com>
2025-02-08 19:27:50 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @SomsPvt
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7826585345:AAETnXbX8BG5Uoj7OVKWik1qqDY-siTD008")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@UknowJoHaN")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1225401466,7702119573")
+
+
+
+
+
+
+
+  
